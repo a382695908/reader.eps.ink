94ab3e6e36b98c0c14a5ab734ee0a89d00872981
eps <epsmail@126.com>
2018-08-14 01:32:58 +0800

    add user-Agent

--- spider/lib/spider.js
+++ spider/lib/spider.js
@@ -3,14 +3,17 @@
  * 封装的spider
  */
 const superagent = require('superagent');
-
+const userAgents = require('../config/userAgents.js');
 let spider = {};
+
+let header = {};
+header['User-Agent'] = userAgents[parseInt(Math.random() * userAgents.length)];
 
 // 请求
 spider.crawl = (url) => {
     return new Promise(function (resolve, reject) {
-        console.log('request '+ url + ' ...');
-        superagent.get(url).end(function (error, content) {
+        console.log('request ' + url + ' ...');
+        superagent.set(header).get(url).end(function (error, content) {
             if (error) {
                 reject(error);
             }
