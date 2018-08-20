1f128f15938abf3228af89890fa0ef95dcf67371
eps <epsmail@126.com>
2018-08-20 15:53:14 +0800

    修改路由指向的controller路径

--- route/route.php
+++ route/route.php
@@ -10,43 +10,43 @@
 // +----------------------------------------------------------------------
 
 // 注册 && 登录 && 退出
-Route::get('/login', 'auth/login_view');
-Route::get('/register', 'auth/register_view');
-Route::post('/login', 'auth/login');
-Route::post('/register', 'auth/register');
-Route::post('/logout', 'auth/logout');
+Route::get('/login', 'Auth/login_view');
+Route::get('/register', 'Auth/register_view');
+Route::post('/login', 'Auth/login');
+Route::post('/register', 'Auth/register');
+Route::post('/logout', 'Auth/logout');
 
 // 分类页面
-Route::get('/category/:cid', 'category/index');
-Route::get('/category/:cid/page/:page', 'category/index');
+Route::get('/category/:cid', 'Category/index');
+Route::get('/category/:cid/page/:page', 'Category/index');
 
 // 全本页面
-Route::get('/full', 'category/isend');
-Route::get('/full/page/:page', 'category/isend');
+Route::get('/full', 'Category/isend');
+Route::get('/full/page/:page', 'Category/isend');
 
 // 小说页面
-Route::get('/novel/:id', 'novel/index');
-Route::get('/novel/search', 'novel/search');
-Route::post('/novel/recommend', 'novel/recommend');
+Route::get('/novel/:id', 'Novel/index');
+Route::get('/novel/search', 'Novel/search');
+Route::post('/novel/recommend', 'Novel/recommend');
 
 // 小说章节阅读界面
-Route::get('/chapter/:id', 'chapter/index');
+Route::get('/chapter/:id', 'Chapter/index');
 
 // 排行榜
-Route::get('/top', 'top/index');
+Route::get('/top', 'Top/index');
 
 // 我的书架
-Route::get('/bookcase', 'bookcase/index');
-Route::post('/bookcase/add', 'bookcase/add');
-Route::post('/bookcase/delete', 'bookcase/delete');
+Route::get('/bookcase', 'Bookcase/index');
+Route::post('/bookcase/add', 'Bookcase/add');
+Route::post('/bookcase/delete', 'Bookcase/delete');
 
 // 访问网站
 Route::get('/ds', 'test/ds');
 
 // 反馈
-Route::post('/advise', 'feedBack/advise');
-Route::post('/report', 'feedBack/report');
-Route::post('/sponsor', 'feedBack/sponsor');
+Route::post('/advise', 'FeedBack/advise');
+Route::post('/report', 'FeedBack/report');
+Route::post('/sponsor', 'FeedBack/sponsor');
 
 
 return [
